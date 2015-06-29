e1a1e0a90edf19917febde334808af0d593c785e
mca312 <angellotti@gmail.com>
2015-06-28 19:10:31 -0500

    Adding top 3 comments

--- public/app/main/mvMainCtrl.js
+++ public/app/main/mvMainCtrl.js
@@ -1,9 +1,11 @@
 angular.module('app').controller('mvMainCtrl', function($scope) {
+
 	$.getJSON('https://www.reddit.com/r/askreddit.json?limit=100', function(data){
 		$scope.threads = data;		
 	})
 	.fail(function(){
-		$scope.threads = 'fail';
+		$scope.question = 'reddit api call failed';
+		$scope.random = 'not loaded';
 	})
 	.done(function() {
 		$scope.nextQuestion();
@@ -11,8 +13,24 @@ angular.module('app').controller('mvMainCtrl', function($scope) {
 	});
 
 	$scope.nextQuestion = function() {
+		$scope.hideAnswerDiv = true;
+
 		var random = Math.floor(Math.random() * 100);
 		$scope.random = random;
-		$scope.question = $scope.threads.data.children[random].data.title
+		$scope.question = $scope.threads.data.children[random].data.title;
+		$scope.currentThreadID = $scope.threads.data.children[random].data.id;
+	}
+
+	$scope.showAnswers = function() {
+		$.getJSON('https://www.reddit.com/r/AskReddit/comments/' + $scope.currentThreadID + '/.json', function(data){
+			$scope.comments = data[1];
+		})
+		.fail(function(){
+			$scope.question = 'reddit comment api call failed';
+		})
+		.done(function() {
+			$scope.hideAnswerDiv = false;
+			$scope.$apply();
+		});
 	}
 });
